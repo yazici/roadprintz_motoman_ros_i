3c9929f5706dba14f30a8c06421e3a46a05682d8
wnewman <wsn@case.edu>
2018-07-03 13:23:03 -0400

    wrote test node to move joints interactively.  Works with robot

---
+++ test_jog_joints/README.md
@@ -0,0 +1,11 @@
+# test_jog_joints
+
+quick test program to move joints of Motoman robot via ROS
+
+## Example usage
+start up robot, enable servos,
+rosrun test_jog_joints test_jog_joints
+respond to prompts
+
+## Running tests/demos
+    
---
+++ test_jog_joints/src/test_jog_joints.cpp
@@ -0,0 +1,304 @@
+//test_jog_joints.cpp
+//wsn, 7/3/18
+
+//some generically useful stuff to include...
+#include <math.h>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+#include <ros/ros.h> //ALWAYS need to include this
+
+//message types used in this example code;  include more message types, as needed
+#include <std_msgs/Bool.h> 
+#include <std_msgs/Float32.h>
+#include <trajectory_msgs/JointTrajectory.h>
+#include <sensor_msgs/JointState.h>
+#include <Eigen/Eigen>
+#include <Eigen/Dense>
+
+
+using namespace std;
+#define VECTOR_DIM 6 // e.g., a 6-dof vector
+const double dt_traj = 0.02; // time step for trajectory interpolation
+int g_done_count = 0;
+int g_done_move = true;
+Eigen::VectorXd g_q_vec_arm_Xd;
+vector<int> g_arm_joint_indices;
+vector<string> g_ur_jnt_names;
+//TEST: deliberately limit joint velocities to very small values
+double g_qdot_max_vec[] = {0.1, 0.1, 0.1, 0.1, 0.1, 0.1}; //put real vel limits here
+int ans;
+
+void map_arm_joint_indices(vector<string> joint_names) {
+    //vector<string> joint_names = joint_state->name;
+    //   vector<string> jnt_names;
+
+    g_arm_joint_indices.clear();
+    int index;
+    int n_jnts = VECTOR_DIM;
+    //cout<<"num jnt names = "<<n_jnts<<endl;
+    std::string j_name;
+
+    for (int j = 0; j < VECTOR_DIM; j++) {
+        j_name = g_ur_jnt_names[j]; //known name, in preferred order
+        for (int i = 0; i < n_jnts; i++) {
+            if (j_name.compare(joint_names[i]) == 0) {
+                index = i;
+                //cout<<"found match at index = "<<i<<endl;
+                g_arm_joint_indices.push_back(index);
+                break;
+            }
+        }
+    }
+    cout << "indices of arm joints: " << endl;
+    for (int i = 0; i < VECTOR_DIM; i++) {
+        cout << g_arm_joint_indices[i] << ", ";
+    }
+    cout << endl;
+}
+
+void set_jnt_names() {
+    g_ur_jnt_names.push_back("joint_1_s");
+    g_ur_jnt_names.push_back("joint_2_l");
+    g_ur_jnt_names.push_back("joint_3_u");
+    g_ur_jnt_names.push_back("joint_4_r");
+    g_ur_jnt_names.push_back("joint_5_b");
+    g_ur_jnt_names.push_back("joint_6_t");
+}
+
+void jointStatesCb(const sensor_msgs::JointState& js_msg) {
+    //joint_states_ = js_msg; // does joint-name mapping only once
+    if (g_arm_joint_indices.size() < 1) {
+        int njnts = js_msg.position.size();
+        ROS_INFO("finding joint mappings for %d jnts", njnts);
+        map_arm_joint_indices(js_msg.name);
+    }
+        for (int i = 0; i < VECTOR_DIM; i++) {
+            g_q_vec_arm_Xd[i] = js_msg.position[g_arm_joint_indices[i]];
+        }
+        cout << "CB: q_vec_arm: " << g_q_vec_arm_Xd.transpose() << endl;
+}
+
+//need to reference realistic joint velocity limits to compute min transition times
+double transition_time(Eigen::VectorXd dqvec) {
+    double t_max = fabs(dqvec[0]) / g_qdot_max_vec[0];
+    //cout<<"qdot max: "<<qdot_max_vec_.transpose()<<endl;
+    double ti;
+    for (int i = 1; i < VECTOR_DIM; i++) {
+        ti = fabs(dqvec[i]) / g_qdot_max_vec[i];
+        if (ti > t_max) t_max = ti;
+    }
+    return t_max;
+}
+
+//given a path, qvecs, comprised of a sequence of 6DOF poses, construct
+// a corresponding trajectory message w/ plausible arrival times
+// re-use joint naming, as set by set_jnt_names
+//arrival time follows from running joints at max feasible velocities...
+// but init vel limits to low values;
+// need to upgrade this to accept an arrival time
+void stuff_trajectory(std::vector<Eigen::VectorXd> qvecs, trajectory_msgs::JointTrajectory &new_trajectory) {
+    //new_trajectory.clear();
+    trajectory_msgs::JointTrajectoryPoint trajectory_point1;
+    //trajectory_msgs::JointTrajectoryPoint trajectory_point2; 
+
+    trajectory_point1.positions.clear();
+
+    new_trajectory.points.clear(); // can clear components, but not entire trajectory_msgs
+    new_trajectory.joint_names.clear();
+    for (int i = 0; i < VECTOR_DIM; i++) {
+        new_trajectory.joint_names.push_back(g_ur_jnt_names[i].c_str());
+    }
+
+    new_trajectory.header.stamp = ros::Time::now();  
+    Eigen::VectorXd q_start, q_end, dqvec, qdot_vec;
+    double del_time;
+    double net_time = 0.0;
+    q_start = qvecs[0];
+    q_end = qvecs[0];
+    qdot_vec.resize(VECTOR_DIM);
+    qdot_vec<<0,0,0,0,0,0;
+    cout<<"stuff_traj: start pt = "<<q_start.transpose()<<endl; 
+    ROS_INFO("stuffing trajectory");
+    //trajectory_point1.positions = qvecs[0];
+
+    trajectory_point1.time_from_start = ros::Duration(net_time);
+    for (int i = 0; i < VECTOR_DIM; i++) { //pre-sizes positions vector, so can access w/ indices later
+        trajectory_point1.positions.push_back(q_start[i]);
+        trajectory_point1.velocities.push_back(0.0);
+    }
+    new_trajectory.points.push_back(trajectory_point1); // first point of the trajectory
+    //add the rest of the points from qvecs
+
+
+    for (int iq = 1; iq < qvecs.size(); iq++) {
+        q_start = q_end;
+        q_end = qvecs[iq];
+        dqvec = q_end - q_start;
+        //cout<<"dqvec: "<<dqvec.transpose()<<endl;
+        del_time = transition_time(dqvec);
+        if (del_time < dt_traj)
+            del_time = dt_traj;
+        //cout<<"stuff_traj: next pt = "<<q_end.transpose()<<endl; 
+        net_time += del_time;
+        //ROS_INFO("iq = %d; del_time = %f; net time = %f",iq,del_time,net_time);        
+        for (int i = 0; i < VECTOR_DIM; i++) { //copy over the joint-command values
+            trajectory_point1.positions[i] = q_end[i];
+            trajectory_point1.velocities[i] = dqvec[i]/del_time;
+        }
+        //trajectory_point1.positions = q_end;
+        trajectory_point1.time_from_start = ros::Duration(net_time);
+        new_trajectory.points.push_back(trajectory_point1);
+    }
+  //display trajectory:
+    for (int iq = 1; iq < qvecs.size(); iq++) {
+        cout<<"traj pt: ";
+                for (int j=0;j<VECTOR_DIM;j++) {
+                    cout<<new_trajectory.points[iq].positions[j]<<", ";
+                }
+        cout<<endl;
+        cout<<"arrival time: "<<new_trajectory.points[iq].time_from_start.toSec()<<endl;
+    }
+}
+
+
+int main(int argc, char** argv) 
+{
+    // ROS set-ups:
+    ros::init(argc, argv, "test_jog_joints"); //node name
+
+    ros::NodeHandle nh; // create a node handle; need to pass this to the class constructor
+    
+    //this is the correct topic and message type for Motoman streaming
+    ros::Publisher pub = nh.advertise<trajectory_msgs::JointTrajectory>("/joint_path_command", 1); 
+     Eigen::VectorXd q_pre_pose;
+    Eigen::VectorXd q_vec_arm;
+    g_q_vec_arm_Xd.resize(VECTOR_DIM);
+
+    std::vector<Eigen::VectorXd> des_path;
+    trajectory_msgs::JointTrajectory des_trajectory; // empty trajectory   
+    set_jnt_names(); //fill a vector of joint names in DH order, from base to tip
+    //here are initial, hard-coded joint angles for arm pose
+    cout << "setting pre-pose: " << endl;
+    q_pre_pose.resize(VECTOR_DIM);
+    q_pre_pose << 0, 0, 0, 0, 0, 0; //-0.907528, -0.111813, 2.06622, 1.8737, -1.295, 2.00164, 0;
+
+    ros::Subscriber joint_state_sub = nh.subscribe("/joint_states", 1, jointStatesCb);
+
+    // warm up the joint-state callbacks; want to make sure the joint states are valid
+    cout << "warming up callbacks..." << endl;
+    while (g_arm_joint_indices.size() < 1) {
+        ros::spinOnce();
+        ros::Duration(1).sleep();
+        ROS_INFO("waiting for joint states...");
+    }
+
+    //get current pose of arm:  
+    cout << "current pose:" << g_q_vec_arm_Xd.transpose() << endl;
+    
+    //q_pre_pose is initialized to all zeros
+    int jnt=5;
+    double qval;
+    while(jnt>=0) {
+        cout<<"enter jnt num, 0 through 5: ";
+        cin>>jnt;
+        cout<<"enter jnt angle: ";
+        cin>>qval;
+        q_pre_pose[jnt] = qval; 
+        des_path.clear();
+        ros::spinOnce(); //update joint states
+        des_path.push_back(g_q_vec_arm_Xd); //start from current pose
+        des_path.push_back(q_pre_pose); //and go to new desired pose
+        stuff_trajectory(des_path, des_trajectory); //convert path to traj
+        //goal.trajectory = des_trajectory; // and put in a goal message
+
+        //ROS_INFO("sending goal to  arm: ");
+        //arm_action_client.sendGoal(goal, &armDoneCb); //ship off goal to action server
+        /*
+        while (g_done_count < 1) { //wait for "done" flag
+            ROS_INFO("waiting to finish move..");
+            ros::spinOnce(); //print jnt angles while waiting
+            cout << "arm is at: " << g_q_vec_arm_Xd.transpose() << endl;
+            ros::Duration(1.0).sleep();
+        }
+        */
+        cout<<"enter 1 to send the trajectory command to move the arm: CAREFUL!!: ";
+        cin>>ans;
+        pub.publish(des_trajectory);
+        
+        ROS_INFO("just kidding--not enabled to publish yet");
+        ros::spinOnce();
+        cout << "arm is at: " << g_q_vec_arm_Xd.transpose() << endl;        
+    }
+
+    return 0;
+}
+    
+    
+    
+     /*
+    
+    //ros::Rate sleep_timer(UPDATE_RATE); //a timer for desired rate to send new traj points as commands
+    trajectory_msgs::JointTrajectory new_trajectory; //   trajectory message
+    trajectory_msgs::JointTrajectoryPoint trajectory_point0; //points to populate the trajectory message
+    trajectory_msgs::JointTrajectoryPoint trajectory_point1;
+    trajectory_msgs::JointTrajectoryPoint trajectory_point2,trajectory_point3,trajectory_point4,trajectory_point5,trajectory_point6; 
+    
+    new_trajectory.points.clear(); //fill in all the joint names
+    
+    new_trajectory.joint_names.push_back("joint_1_s");
+    new_trajectory.joint_names.push_back("joint_2_l");
+    new_trajectory.joint_names.push_back("joint_3_u");
+    new_trajectory.joint_names.push_back("joint_4_r");
+    new_trajectory.joint_names.push_back("joint_5_b");
+    new_trajectory.joint_names.push_back("joint_6_t");    
+
+    ros::Rate sleep_timer(1.0); //1Hz update rate
+    
+    // build an example trajectory:
+    trajectory_point0.positions.clear();
+    trajectory_point1.positions.clear();    
+    trajectory_point2.positions.clear();    
+    
+    //specify start pose (all zeros) and two points:
+    for (int ijnt=0;ijnt<6;ijnt++) {
+        trajectory_point0.positions.push_back(0.0); //all zeros
+        trajectory_point1.positions.push_back(0.0); // 
+        trajectory_point2.positions.push_back(0.0); // stuff in position commands for 6 joints   
+        trajectory_point3.positions.push_back(0.0);
+        trajectory_point4.positions.push_back(0.0);
+        trajectory_point5.positions.push_back(0.0);
+        trajectory_point6.positions.push_back(0.0); 
+        
+    //specify arrival time (from start of trajectory)
+    trajectory_point0.time_from_start = ros::Duration(1.0); //1 second to arrive
+    
+    //put this point into trajectory message
+    new_trajectory.points.clear();
+    new_trajectory.header.stamp = ros::Time::now();
+    new_trajectory.points.push_back(trajectory_point0); //contains a single point--home
+    
+    ROS_INFO("sending robot to home position: ");
+    int npts = new_trajectory.points.size();
+    ROS_INFO("num pts in initial traj = %d",npts);
+    for (int i=0;i<3;i++) //send this twice, just to make sure have communication w/ subscriber
+    {
+        pub.publish(new_trajectory);
+            ros::spinOnce();
+            ros::Duration(1.0).sleep();
+    }
+    
+    npts = new_trajectory.points.size(); 
+     int njnts = new_trajectory.points[0].positions.size();
+    //ROS_INFO("sending a trajectory with %d poses, each with %d joints ",npts,njnts);
+    //pub.publish(new_trajectory);
+    //don't die yet--make sure message gets received
+     for (int i=0;i<3;i++) {
+            sleep_timer.sleep();
+     }
+
+    return 0;
+}             
+*/
+
